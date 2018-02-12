cdb1c55b288c4380897e0260331a9d8ae6bb73c2
Trevor Meadows <tlm04070@gmail.com>
2018-02-11 22:21:35 -0500

    added mlab connection and cookie session, finished oauth process

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,14 @@ passport.use(
       callbackURL: "http://localhost:5000/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile:", profile);
+      console.log(profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          User.create({ googleId: profile.id }).then(user => done(null, user));
+        }
+      });
     }
   )
 );
