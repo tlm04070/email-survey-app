91d302203e2dc9f3a522418962e2591384beeaf8
Trevor Meadows <tlm04070@gmail.com>
2018-02-15 06:43:29 -0500

    working on header conditionally rendering login/logout with redirects

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
-
+import { connect } from "react-redux";
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
@@ -8,15 +26,14 @@ class Header extends Component {
           <a className="left brand-logo" href="/">
             Emaily
           </a>
-          <ul className="right">
-            <li>
-              <a>Login in with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
+function mapStateToProps({ auth }) {
+  return { auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
