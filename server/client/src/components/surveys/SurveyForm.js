d3044e294d1c6b91d09aedd57746cd54a595b2e6
Trevor Meadows <tlm04070@gmail.com>
2018-03-13 19:38:59 -0400

    finished survey redux form input and validation

---
+++ server/client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,81 @@
+//SurveyForm shows a form for a user to add input
+import _ from "lodash";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
+import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
+
+const FIELDS = [
+  {
+    label: "Survey Title",
+    name: "title",
+    noValueError: "Please provide a title for your survey!"
+  },
+  {
+    label: "Subject Line",
+    name: "subject",
+    noValueError: "Please enter a subject line"
+  },
+  {
+    label: "Email Body",
+    name: "body",
+    noValueError: "Body of the email is required for surveys"
+  },
+  {
+    label: "Recipient List",
+    name: "emails",
+    noValueError: "Please enter a list of emails to recieve the survey"
+  }
+];
+
+class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || "");
+
+  _.each(FIELDS, ({ name, noValueError }) => {
+    if (!values[name]) {
+      errors[name] = noValueError;
+    }
+  });
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: "surveyForm"
+})(SurveyForm);
